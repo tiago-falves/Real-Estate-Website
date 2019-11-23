1fdf95ab7ea1ebc9b520d8c5813e0e0aef7b5074
tiago-falves <tiago.falves98@gmail.com>
2019-11-23 15:59:27 +0100

    Nada de jeito

--- Imobiliaria/main_page.php
+++ Imobiliaria/main_page.php
@@ -1,25 +1,7 @@
-<!DOCTYPE html>
-<html lang="en-US">
-    <head>
-        <title>Invicta</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        <header>
-            <h1> <a href="main_page.html">Invicta</a></h1>
-            <div id="signup">
-                <a href="register.html">Sign Up</a>
-                <a href="login.html">Login</a>
-            </div>
-            <nav id="menu">
-                <ul>
-                    <li><a href="buy.html">Buy</a></li>
-                    <li><a href="rent.html">Rent</a></li>
-                    <li><a href="discover.html">Rent</a></li>
-                </ul>
-            </nav>       
-        </header>
+<?php
+  include_once('../templates/Header.html');
+?>
+
         <div id="Search">
             <img src="../Images/ribeira.jpg" alt="Ribeira">
             <ul id="Filters">
