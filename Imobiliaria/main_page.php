7073c7b8a59ec20d63937dcb1a1ef1154e2b3b08
tiago-falves <tiago.falves98@gmail.com>
2019-11-23 21:06:30 +0100

    Started css

--- Imobiliaria/main_page.php
+++ Imobiliaria/main_page.php
@@ -1,52 +1,60 @@
 <?php
-  include_once('../templates/Header.html');
+    include_once('../templates/common/Header.html');
 ?>
-
         <div id="Search">
+            <section id="SearchText">
+                <ul id="Filters">
+                    <li><a href="">For Sale</a></li>
+                    <li><a href="">Rent</a></li>
+                    <li><a href="">Zaaaaaas</a></li>
+                </ul>
+                <input type="search" name="searchHouses" value="Search...">
+                <section id = "SearchButton">
+                    <!-- <a href="searchResults.txt"><img src="../Images/search.png" alt="Search Logo"></a>   -->
+                </section>
+            </section>
             <img src="../Images/ribeira.jpg" alt="Ribeira">
-            <ul id="Filters">
-                <li><a href="">For Sale</a></li>
-                <li><a href="">Rent</a></li>
-                <li><a href="">Zaaaaaas</a></li>
-            </ul>
-            <input type="search" name="searchHouses" value="Search...">
-            <a href="searchResults.txt"><img src="../Images/search.png" alt="Search Logo"></a>  
+            
         </div>
         <div id="BestOffers">
-            <h2>Best Offers</h2>
-            <section>
-                <header>
-                    <h3>Price</h3>
-                    <h4>City</h4>
-                </header>
-                <a href="home.html"><img src="../Images/home.jpg" alt="Casa 1"></a>
+            <header>
+                <h2>Best Offers</h2>
+            </header>
+            <section id = "Homes">
+                <section class = "Home">
+                    <header>
+                        <h3>Price</h3>
+                        <h4>City</h4>
+                    </header>
+                    <a href="home.html"><img src="../Images/home.jpg" alt="Casa 1"></a>
+                </section>
+                <section class = "Home">
+                    <header>
+                        <h3>Price</h3>
+                        <h4>City</h4>
+                    </header>
+                <a href="home.html"><img src="../Images/home.jpg" alt="Casa 2"></a>                </section>
+                <section class = "Home">
+                    <header>
+                        <h3>Price</h3>
+                        <h4>City</h4>
+                    </header>
+                <a href="home.html"><img src="../Images/home.jpg" alt="Casa 3"></a>                </section>
+                <section class = "Home">
+                    <header>
+                        <h3>Price</h3>
+                        <h4>City</h4>
+                    </header>
+                    <a href="home.html"><img src="../Images/home.jpg" alt="Casa 4"></a>                </section>
+                <section class = "Home">
+                    <header>
+                        <h3>Price</h3>
+                        <h4>City</h4>
+                    </header>
+                    <a href="home.html"><img src="../Images/home.jpg" alt="Casa 5"></a>                
+                </section>   
             </section>
-            <section>
-                <header>
-                    <h3>Price</h3>
-                    <h4>City</h4>
-                </header>
-            <a href="home.html"><img src="../Images/home.jpg" alt="Casa 2"></a>                </section>
-            <section>
-                <header>
-                    <h3>Price</h3>
-                    <h4>City</h4>
-                </header>
-            <a href="home.html"><img src="../Images/home.jpg" alt="Casa 3"></a>                </section>
-            <section>
-                <header>
-                    <h3>Price</h3>
-                    <h4>City</h4>
-                </header>
-                 <a href="home.html"><img src="../Images/home.jpg" alt="Casa 4"></a>                </section>
-            <section>
-                <header>
-                    <h3>Price</h3>
-                    <h4>City</h4>
-                </header>
-                <a href="home.html"><img src="../Images/home.jpg" alt="Casa 5"></a>                
-            </section>   
-            <a href="">See All</a>            
+            <button type="button">See All</button>         
         </div>
         <footer>
             <h3>Invicta</h3>
