40ea98419d46c7ec3e67d697ca80a5b72e8c3a12
tiago-falves <tiago.falves98@gmail.com>
2019-11-24 18:31:41 +0100

    Added more css

--- Imobiliaria/main_page.php
+++ Imobiliaria/main_page.php
@@ -4,7 +4,7 @@
         <title>Invicta</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="../Css/style.css" rel="stylesheet">
+        <link href="../Css/styles.css" rel="stylesheet">
         <link href="../Css/layout.css" rel="stylesheet">
         <link href="../Css/forms.css" rel="stylesheet">
     </head>
@@ -32,7 +32,7 @@
                 </ul>
                 <input type="search" name="searchHouses" value="Search...">
                 <section id = "SearchButton">
-                    <!-- <a href="searchResults.txt"><img src="../Images/search.png" alt="Search Logo"></a>   -->
+                    <a href="searchResults.txt"><img src="../Images/search.png" alt="Search Logo"></a>  
                 </section>
             </section>
             <img src="../Images/ribeira.jpg" alt="Ribeira">
@@ -43,7 +43,7 @@
                 <h2>Best Offers</h2>
             </header>
             <section id = "Homes">
-                <section class = "Home">
+                <section class = "Home" id= "BiggerPhoto">
                     <header>
                         <h3>Price</h3>
                         <h4>City</h4>
