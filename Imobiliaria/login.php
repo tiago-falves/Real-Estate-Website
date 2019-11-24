9db9f223a0c1823ec2f88e486880ed2b4e032375
tiago-falves <tiago.falves98@gmail.com>
2019-11-24 16:12:18 +0100

    Login Css

--- Imobiliaria/login.php
+++ Imobiliaria/login.php
@@ -4,36 +4,40 @@
         <title>Invicta</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../Css/forms.css" rel="stylesheet">
+        <link href="../Css/style.css" rel="stylesheet">
+        <link href="../Css/layout.css" rel="stylesheet">
+
     </head>
     <body>
         <header>
-            <h1> <a href="main_page.html">Invicta</a></h1>
+            <h1> <a href="main_page.php">Invicta</a></h1>
             <div id="signup">
-                <a href="register.html">Sign Up</a>
-                <a href="login.html">Login</a>
+                <a href="register.php">Sign Up</a>
+                <a href="login.php">Login</a>
             </div>
             <nav id="menu">
                 <ul>
-                    <li><a href="buy.html">Buy</a></li>
-                    <li><a href="rent.html">Rent</a></li>
-                    <li><a href="discover.html">Rent</a></li>
+                    <li><a href="buy.php">Buy</a></li>
+                    <li><a href="rent.php">Rent</a></li>
+                    <li><a href="discover.php">Rent</a></li>
                 </ul>
             </nav>       
         </header>
     <section id="login">
-      <h2>Login</h2>
-      <form>
-        <label>
-          Username <input type="text" name="username">
-        </label>
-        <label>
-          Password <input type="password" name="password">
-        </label>
-        <input type="checkbox"  value="Remember me"> Remember me<br>
+      
+      <header>
+        <h2>Login</h2>
+      </header>
+      
+      <form method="post" action="action_login.php">
+        <input type="text" name="username" placeholder="username" required>
+        <input type="password" name="password" placeholder="password" required>
         <input type="submit" value="Login">
-        <span class = "Forgot Password"><a href="edit_profile-html"> Frogot Password</a></span>
-
       </form>
+      <footer>
+        <p>Don't have an account? <a href="signup.php">Signup!</a></p>
+      </footer>
     </section>
     <footer>
       <p>&copy; Invicta, 2019</p>
--- Imobiliaria/main_page.php
+++ Imobiliaria/main_page.php
@@ -1,6 +1,28 @@
-<?php
-    include_once('../templates/common/Header.html');
-?>
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Invicta</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../Css/style.css" rel="stylesheet">
+        <link href="../Css/layout.css" rel="stylesheet">
+        <link href="../Css/forms.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1> <a href="main_page.php">Invicta</a></h1>
+            <div id="signup">
+                <a href="register.php">Sign Up</a>
+                <a href="login.php">Login</a>
+            </div>
+            <nav id="menu">
+                <ul>
+                    <li><a href="buy.php">Buy</a></li>
+                    <li><a href="rent.php">Rent</a></li>
+                    <li><a href="discover.php">Rent</a></li>
+                </ul>
+            </nav>       
+        </header>
         <div id="Search">
             <section id="SearchText">
                 <ul id="Filters">
