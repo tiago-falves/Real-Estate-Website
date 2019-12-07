ab5b1c27428dac49748548db2f914b3122f3b602
tiago-falves <tiago.falves98@gmail.com>
2019-12-07 21:37:22 +0100

    Tried to add register option

--- Imobiliaria/register.php
+++ Imobiliaria/register.php
@@ -1,4 +1,5 @@
 <?php
+  include_once('../session/session.php');
   include_once('../templates/common/Header.php');  
   include_once('../templates/user/register.php');
   include_once('../templates/common/Footer.php');  
--- templates/user/register.php
+++ templates/user/register.php
@@ -4,14 +4,26 @@
     <h2>Register</h2>
   </header>
   
-  <form method="post" action="action_login.php">
+  <form method="post" action="../Actions/action_signup.php">
     <input type="email" name="emailaddress" placeholder="E-mail Adress">
     <input type="text" name="username" placeholder="username" required>
     <input type="password" name="password" placeholder="password" required>
     <input type="password" name="confirm password" placeholder="confirm password" required>
-    <input type="submit" value="Login">
+    <input type="submit" value="Register">
   </form>
   <footer>
-    <p>Don't have an account? <a href="signup.php">Signup!</a></p>
+    <p> <a href="login.php"> Already have an account? Login!</a></p>
+    <section id="messages">
+      <?php $errors = getErrorMessages();foreach ($errors as $error) { ?>
+      <article class="error">
+        <p><?=$error?></p>
+      </article>
+      <?php } ?>
+      <?php $successes = getSuccessMessages();foreach ($successes as $success) { ?>
+      <article class="success">
+        <p><?=$success?></p>
+      </article>
+      <?php } clearMessages(); ?>
+  </section>
   </footer>
 </section>
