b4ebfd1b23d8383aa5ba74a91d551d915d6ed73d
tiago-falves <tiago.falves98@gmail.com>
2019-12-10 23:38:42 +0100

    Now the discover page goes and gets the information from the database

---
+++ templates/Homes/homeFunctions.php
@@ -0,0 +1,34 @@
+<?php function draw_homes($homes) { ?>
+
+  <div id="ListHouses">
+    <h2>Homes</h2>
+    <?php 
+      foreach ($homes as $home)
+        draw_home($home);
+    ?>
+    <a href="">See All</a>            
+  </div>
+
+<?php } ?>
+
+
+
+<?php function draw_home($home) {
+//Porque que ele nao esta a desenhar todos os parametros???>
+  <section class = House>
+    <header>
+        <h3><?$home['title']?></h3>
+        <h4>Price: <?$home['price']?> $</h4>
+    </header>
+    <p><?$home['description']?></p>
+    <button onclick="myFunction()" id="seeMore2">More Info</button>
+    <footer>
+        <span class="author"><?$home['owner'] /*Vai ter que ter aqui uma funçao get owner by id*/?></span>
+    </footer>
+    <a href="home.html"><img src="../Images/home.jpg" alt="Casa 1"></a>
+  </section>   
+<?php } ?>
+
+
+
+
