664117ad90c40c72266db08a94b274243e838caa
João Macedo <mac.j25@gmail.com>
2019-12-12 17:25:03 +0000

    Css started work on horizontal list scrolling

--- templates/Homes/homes.php
+++ templates/Homes/homes.php
@@ -1,43 +1,45 @@
 <section id = "Homes">
-    <div id = "BiggerPhoto">
-        <section class = "Home">
-            <header>
-                <h3>300209€</h3>
-                <h4>Felgueiras</h4>
-            </header>
-            <a href="home.php"><img src="../Images/home.jpg" alt="Casa 1"></a>
-        </section>
-    </div>
-    <div class = "SmallerPhotosCollumn">
-        <section class = "Home">
-            <header>
-                <h3>7€</h3>
-                <h4>Rabo de Peixe</h4>
-            </header>
-            <a href="home.php"><img src="../Images/home.jpg" alt="Casa 2"></a>
-        </section>
-        <section class = "Home">
-            <header>
-                <h3>80390€</h3>
-                <h4>Pingo Doce</h4>
-            </header>
-            <a href=""><img src="../Images/home.jpg" alt="Casa 3"></a>                
-        </section>
-    </div>
-    <div class = "SmallerPhotosCollumn">
-        <section class = "Home">
-            <header>
-                <h3>2000$</h3>
-                <h4>Kenya</h4>
-            </header>
-            <a href="home.php"><img src="../Images/home.jpg" alt="Casa 3"></a>
-        </section>
-        <section class = "Home">
-            <header>
-                <h3>Price</h3>
-                <h4>City</h4>
-            </header>
-            <a href="home.php"><img src="../Images/home.jpg" alt="Casa 4"></a>                
-        </section>
+    <div id = "ScrolableList">
+        <div id = "BiggerPhoto">
+            <section class = "Home">
+                <header>
+                    <h3>300209€</h3>
+                    <h4>Felgueiras</h4>
+                </header>
+                <a href="home.php"><img src="../Images/home.jpg" alt="Casa 1"></a>
+            </section>
+        </div>
+        <div class = "SmallerPhotosCollumn">
+            <section class = "Home">
+                <header>
+                    <h3>700000000€</h3>
+                    <h4>Rabo de Peixe</h4>
+                </header>
+                <a href="home.php"><img src="../Images/home.jpg" alt="Casa 2"></a>
+            </section>
+            <section class = "Home">
+                <header>
+                    <h3>80390€</h3>
+                    <h4>Pingo Doce</h4>
+                </header>
+                <a href=""><img src="../Images/home.jpg" alt="Casa 3"></a>                
+            </section>
+        </div>
+        <div class = "SmallerPhotosCollumn">
+            <section class = "Home">
+                <header>
+                    <h3>2000$</h3>
+                    <h4>Kenya</h4>
+                </header>
+                <a href="home.php"><img src="../Images/home.jpg" alt="Casa 3"></a>
+            </section>
+            <section class = "Home">
+                <header>
+                    <h3>Price</h3>
+                    <h4>City</h4>
+                </header>
+                <a href="home.php"><img src="../Images/home.jpg" alt="Casa 4"></a>                
+            </section>
+        </div>
     </div>
 </section>
