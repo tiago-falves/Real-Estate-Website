0b63e9d64ca42b8a375871d36df55f5feec6646d
BrunoFCM <bruno_micaelo@hotmail.com>
2019-12-13 16:13:09 +0000

    Fixed the edit profile action

--- Actions/action_change_profile.php
+++ Actions/action_change_profile.php
@@ -1,33 +1,33 @@
 <?php
- include_once('../session/session.php');
- include_once('../database/database.php');
- include_once('../database/queries.php');
+  include_once('../session/session.php');
+  include_once('../database/database.php');
+  include_once('../database/queries.php');
+  include_once('../database/inserts.php');
 
+  $username = $_SESSION['username'];
+  /*
+  $password = $_POST['password'];
+
+  if (!checkUserCredentials($username, $password)) {
+    $_SESSION['error_messages'][] = "Invalid password";
+    die(header('Location: ' . $_SERVER['HTTP_REFERER']));
+  }
+
+  $new_password = $_POST['newPassword'];
+  $comfirmed_password = $_POST['confirmedPassword'];
+
+  if(strcmp($new_password, $confirmedPassword) != 0){
+    $_SESSION['error_messages'][] = "New password does not match the confirmation";
+    die(header('Location: ' . $_SERVER['HTTP_REFERER']));
+  }
+  */
+
+  $user = getUserFromUserName($username);
+  $new_title = $_POST['title'];
+  $new_location = getLocationFromName($_POST['location']);
+  $new_description = $_POST['description'];
   
-  //Como assim quando nao estou logged in ele retorna o meu up
-  
-  
-function updateUser($username,$newPassord,$newDescription){
-    $db = Database::instance()->db();
-    $stmt = $db->prepare(' UPDATE PERSON SET PASSWORD_HASH = ? , USERDESCRIPTION = ? WHERE userName = ?');
-    $stmt->execute(array(password_hash($newPassord, PASSWORD_DEFAULT),$newDescription,$username));
-}
-
-
-
-
-if (checkUserCredentials($_SESSION['username'], $_POST['password'])) {
-    updateUser($username,$_POST['password'],$_POST['description']);
-    $_SESSION['success_messages'][] = "Profile changed successfully!";
-
-} else {
-    echo("Para de tentar entrar no site oh boi");
-    $_SESSION['error_messages'][] = "Wrong information";
-}
-
-
+  updateUserProfile($user['id'], $new_title, $new_location['id'], $new_description);
 
   header('Location: ' . $_SERVER['HTTP_REFERER']);
-
-
 ?>
