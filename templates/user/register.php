051ca576d3f02b910808be81555f8257d82eb275
BrunoFCM <bruno_micaelo@hotmail.com>
2019-12-14 15:34:37 +0000

    Added simple script to the register page

--- templates/user/register.php
+++ templates/user/register.php
@@ -4,13 +4,14 @@
     <h2>Register</h2>
   </header>
   
-  <form method="post" action="../Actions/action_signup.php">
-    <input type="email" name="emailaddress" placeholder="E-mail Adress" required>
-    <input type="text" name="username" placeholder="username" required>
-    <input type="password" name="password" placeholder="password" required>
-    <input type="password" name="confirm_password" placeholder="confirm password" required>
+  <form id="registerForm" method="post" action="../Actions/action_signup.php">
+    <p>Email: <input type="email" name="emailaddress" placeholder="E-mail Adress" required></p>
+    <p>User name: <input type="text" name="username" placeholder="username" required></p>
+    <p>Password: <input type="password" name="password"></p>
+    <p>Confirm Password: <input type="password" name="confirm_password"></p>
     <input type="submit" value="Register">
   </form>
+  <script src="../Scripts/registerForm.js"></script>
   <footer>
     <p> <a href="register.php"> Already have an account? Login!</a></p>
     <section id="messages">
